0aa8e603913dd8a7f6ecad207a62a2cc6053a3d1
Rohan Vivek <150229721+rohan-vivek@users.noreply.github.com>
2024-03-20 20:27:45 +0530

    Update main.tf

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -381,7 +381,7 @@ variable "node_group_depends_on" {
   # the value doesn't matter; we're just using this variable
   # to propagate dependencies.
   type    = any
-  default = []
+  # default = []
 }
 
 
